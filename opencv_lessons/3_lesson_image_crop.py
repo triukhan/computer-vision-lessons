91b9e7b430ff095a2672c1643406b850ec6556c8
triukhan <danylotriukhan@gmail.com>
2025-12-22 22:48:22 +0200

    third lesson

---
+++ opencv_lessons/3_lesson_image_crop.py
@@ -0,0 +1,15 @@
+import os
+import cv2
+
+
+image_path = os.path.join('opencv_lessons', 'data', 'img.png')
+image = cv2.imread(image_path)
+
+print(image.shape)
+
+cropped_image = image[230:450, 200:400]
+
+print(cropped_image.shape)
+
+cv2.imshow('img', cropped_image)
+cv2.waitKey(0)
---
+++ opencv_lessons/3_lesson_image_resize.py
@@ -0,0 +1,14 @@
+import os
+import cv2
+
+
+image_path = os.path.join('opencv_lessons', 'data', 'img.png')
+image = cv2.imread(image_path)
+
+resized_image = cv2.resize(image, (300, 500))
+
+print(image.shape)
+print(resized_image.shape)
+
+cv2.imshow('img', resized_image)
+cv2.waitKey(0)
