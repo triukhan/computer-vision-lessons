7aba14020a938dcc23876cea81116be6600c7025
triukhan <danylotriukhan@gmail.com>
2025-12-26 20:06:17 +0200

    eleventh lesson

---
+++ opencv_lessons/lesson_11/lesson_11_face_detection.py
@@ -0,0 +1,81 @@
+import ctypes
+import os
+import mediapipe as mp
+import cv2
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from typing import Tuple, Union
+import math
+import cv2
+import numpy as np
+
+MARGIN = 10
+ROW_SIZE = 10
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+TEXT_COLOR = (255, 0, 0)
+MODEL_PATH = 'opencv_lessons/lesson_11/blaze_face_short_range.tflite'
+
+
+def _normalized_to_pixel_coordinates(
+        normalized_x: float, normalized_y: float, image_width: int, image_height: int
+) -> Union[None, Tuple[int, int]]:
+
+    def is_valid_normalized_value(value: float) -> bool:
+        return (value > 0 or math.isclose(0, value)) and (value < 1 or math.isclose(1, value))
+
+    if not (is_valid_normalized_value(normalized_x) and is_valid_normalized_value(normalized_y)):
+        return None
+
+    x_px = min(math.floor(normalized_x * image_width), image_width - 1)
+    y_px = min(math.floor(normalized_y * image_height), image_height - 1)
+
+    return x_px, y_px
+
+
+def visualize(img, detection_res) -> np.ndarray:
+    annotated_img = img.copy()
+
+    for detection in detection_res.detections:
+        bbox = detection.bounding_box
+        start_point = bbox.origin_x, bbox.origin_y
+        end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
+        cv2.rectangle(annotated_img, start_point, end_point, TEXT_COLOR, 3)
+
+        category = detection.categories[0]
+        category_name = category.category_name
+        category_name = '' if category_name is None else category_name
+        probability = round(category.score, 2)
+        result_text = category_name + ' (' + str(probability) + ')'
+        text_location = (MARGIN + bbox.origin_x,
+                         MARGIN + ROW_SIZE + bbox.origin_y)
+        cv2.putText(annotated_img, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                    FONT_SIZE, TEXT_COLOR, FONT_THICKNESS)
+
+    return annotated_img
+
+
+cap = cv2.VideoCapture(1)
+
+while True:
+    ret, frame = cap.read()
+    base_options = python.BaseOptions(model_asset_path=MODEL_PATH)
+    options = vision.FaceDetectorOptions(base_options=base_options, running_mode=vision.RunningMode.IMAGE)
+    detector = vision.FaceDetector.create_from_options(options)
+
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    height, width, channels = frame_rgb.shape
+    data_ptr = frame_rgb.ctypes.data_as(ctypes.POINTER(ctypes.c_uint8))
+
+    image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
+
+    detection_result = detector.detect(image)
+    annotated_image = visualize(image.numpy_view(), detection_result)
+    rgb_annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
+    cv2.imshow('rgb_annotated_image', rgb_annotated_image)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
