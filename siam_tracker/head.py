8d3c9e1fb57af22ded6e27aec2a808d050f4ba69
Danylo Triukhan <116063228+triukhan@users.noreply.github.com>
2026-04-07 17:52:06 +0300

    Create head.py

---
+++ siam_tracker/head.py
@@ -0,0 +1,38 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def xcorr_fast(x, kernel):
+    """group conv2d to calculate cross correlation, fast version
+    """
+    batch = kernel.size()[0]
+    pk = kernel.view(-1, x.size()[1], kernel.size()[2], kernel.size()[3])
+    px = x.view(1, -1, x.size()[2], x.size()[3])
+    po = F.conv2d(px, pk, groups=batch)
+    po = po.view(batch, -1, po.size()[2], po.size()[3])
+    return po
+
+
+
+class BAN(nn.Module):
+    def __init__(self):
+        super(BAN, self).__init__()
+
+    def forward(self, z_f, x_f):
+        raise NotImplementedError
+
+
+class UPChannelBAN(BAN):
+    def __init__(self, feature_in: int = 96):
+        super(UPChannelBAN, self).__init__()
+        loc_output = 4
+        self.template_loc_conv = nn.Conv2d(feature_in, feature_in * loc_output, kernel_size=3)
+        self.search_loc_conv = nn.Conv2d(feature_in, feature_in, kernel_size=3)
+        self.loc_adjust = nn.Conv2d(loc_output, loc_output, kernel_size=1)
+
+    def forward(self, z_f, x_f):
+        loc_kernel = self.template_loc_conv(z_f)
+        loc_feature = self.search_loc_conv(x_f)
+
+        loc = self.loc_adjust(xcorr_fast(loc_feature, loc_kernel))
+        return loc
