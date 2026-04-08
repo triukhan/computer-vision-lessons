a7d0e6ce235aaf783c47cd4da7cdbf93d054673f
Danylo Triukhan <116063228+triukhan@users.noreply.github.com>
2026-04-08 17:53:26 +0300

    Update track_pp.py

--- siam_tracker/track_pp.py
+++ siam_tracker/track_pp.py
@@ -3,8 +3,7 @@ from pathlib import Path
 import cv2
 
 from siam_tracker.model_builder import ModelBuilder
-from siam_tracker.tracker import SiamTracker
-
+from siam_tracker.tracker import NanoTracker
 
 BASE_DIR = Path(__file__).resolve().parent
 PROJECT_ROOT = BASE_DIR.parent
@@ -14,7 +13,7 @@ def track_object(video_path: Path, stop=False):
     video = cv2.VideoCapture(video_path)
     video.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
-    tracker = SiamTracker(ModelBuilder())
+    tracker = NanoTracker(ModelBuilder())
     cv2.namedWindow('tracking with siam')
     cv2.setMouseCallback('tracking with siam', tracker.on_mouse)
     # tracker.on_mouse(cv2.EVENT_LBUTTONDOWN, 166, 221, None, None)
@@ -28,8 +27,8 @@ def track_object(video_path: Path, stop=False):
             tracker.init(frame, tracker.bbox)
             tracker.need_init = False
 
-        if tracker.in_track:
-            tracker.bbox = tracker.track(frame, tracker.bbox)
+        if tracker.center_pos is not None:
+            tracker.bbox = tracker.track(frame)['bbox']
             cx, cy, w, h = tracker.bbox
             cv2.rectangle(
                 frame, (int(cx - w // 2), int(cy - h // 2)), (int(cx + w // 2), int(cy + h // 2)), (0, 0, 255), 1
