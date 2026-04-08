60e6a44d89b8e8c1fa26932c7c6ab364d53a786b
Danylo Triukhan <116063228+triukhan@users.noreply.github.com>
2026-04-08 17:53:57 +0300

    Update tracker.py

--- siam_tracker/tracker.py
+++ siam_tracker/tracker.py
@@ -7,48 +7,102 @@ import torch
 
 BASE_DIR = Path(__file__).resolve().parent
 PROJECT_ROOT = BASE_DIR.parent
-MODEL_PATH = PROJECT_ROOT / 'BaselinePretrained.pth.tar'
+MODEL_PATH = PROJECT_ROOT / 'mobilenetv3_small_1.0.pth'
 H, W = 60, 60
 
 
-def get_subwindow(frame, bbox, size: int, context_factor: float = 0.5):
-    cx, cy, w, h = bbox
+# def get_subwindow_tracking(frame, target_pos, model_size: int, original_size: int, channel_avg):
+#     c = (original_size + 1) / 2
+#
+#     context_xmin = round(target_pos[0] - c)
+#     context_xmax = context_xmin + original_size - 1
+#     context_ymin = round(target_pos[1] - c)
+#     context_ymax = context_ymin + original_size - 1
+#
+#     left_pad = int(max(0, -context_xmin))
+#     top_pad = int(max(0, -context_ymin))
+#     right_pad = int(max(0, context_xmax - frame.shape[1] + 1))
+#     bottom_pad = int(max(0, context_ymax - frame.shape[0] + 1))
+#
+#     # if edge of the frame - add black pixels instead
+#     context_xmin += left_pad
+#     context_xmax += left_pad
+#     context_ymin += top_pad
+#     context_ymax += top_pad
+#
+#     if any([left_pad, top_pad, right_pad, bottom_pad]):
+#         te_im = cv2.copyMakeBorder(
+#             frame, top=top_pad, bottom=bottom_pad, left=left_pad, right=right_pad, borderType=cv2.BORDER_CONSTANT, value=channel_avg
+#         )
+#         im_path_original = te_im[
+#             context_ymin:context_ymax + 1,
+#             context_xmin:context_xmax + 1
+#         ]
+#     else:
+#         im_path_original = frame[context_ymin:context_ymax + 1, context_xmin:context_xmax + 1]
+#
+#     im_path = cv2.resize(im_path_original, (model_size, model_size))
+#
+#     return im_path
 
-    # expand bbox with contex factor
-    context = context_factor * (w + h)
-    crop_size = int(np.sqrt((w + context) * (h + context)))  # sqrt saves proportions
-    channel_average = np.mean(frame, axis=(0, 1))
+def get_subwindow_tracking(im, pos, model_sz, original_sz, avg_chans):
+    """
+    args:
+        im: bgr based image
+        pos: center position
+        model_sz: exemplar size
+        s_z: original size
+        avg_chans: channel average
+    """
+    if isinstance(pos, float):
+        pos = [pos, pos]
+    sz = original_sz
+    im_sz = im.shape
+    c = (original_sz + 1) / 2
+    # context_xmin = round(pos[0] - c) # py2 and py3 round
+    context_xmin = np.floor(pos[0] - c + 0.5)
+    context_xmax = context_xmin + sz - 1
+    # context_ymin = round(pos[1] - c)
+    context_ymin = np.floor(pos[1] - c + 0.5)
+    context_ymax = context_ymin + sz - 1
+    left_pad = int(max(0., -context_xmin))
+    top_pad = int(max(0., -context_ymin))
+    right_pad = int(max(0., context_xmax - im_sz[1] + 1))
+    bottom_pad = int(max(0., context_ymax - im_sz[0] + 1))
 
-    x1, y1 = int(cx - crop_size / 2), int(cy - crop_size / 2)
-    x2, y2 = int(cx + crop_size / 2), int(cy + crop_size / 2)
+    context_xmin = context_xmin + left_pad
+    context_xmax = context_xmax + left_pad
+    context_ymin = context_ymin + top_pad
+    context_ymax = context_ymax + top_pad
 
-    # if edge of the frame - add black pixels instead
-    left, right = max(0, -x1), max(0, x2 - frame.shape[1])
-    top, bottom = max(0, -y1), max(0, y2 - frame.shape[0])
-
-    x1, y1 = max(0, x1), max(0, y1)  # clamping after expanding
-    x2, y2 = min(frame.shape[1], x2), min(frame.shape[0], y2)
-
-    r, c, k = frame.shape
-    if any([top, bottom, left, right]):
-        pad_shape = (r + top + bottom, c + left + right, k)
-        te_image = np.zeros(pad_shape, np.uint8)
-        te_image[top:top + r, left:left + c, :] = frame
-        if top:
-            te_image[0:top, left:left + c, :] = channel_average
-        if bottom:
-            te_image[r + top:, left:left + c, :] = channel_average
-        if left:
-            te_image[:, 0:left, :] = channel_average
-        if right:
-            te_image[:, c + left:, :] = channel_average
-        image_patch = te_image[int(y1):int(y2), int(x1):int(x2), :]
+    r, c, k = im.shape
+    if any([top_pad, bottom_pad, left_pad, right_pad]):
+        size = (r + top_pad + bottom_pad, c + left_pad + right_pad, k)
+        te_im = np.zeros(size, np.uint8)
+        te_im[top_pad:top_pad + r, left_pad:left_pad + c, :] = im
+        if top_pad:
+            te_im[0:top_pad, left_pad:left_pad + c, :] = avg_chans
+        if bottom_pad:
+            te_im[r + top_pad:, left_pad:left_pad + c, :] = avg_chans
+        if left_pad:
+            te_im[:, 0:left_pad, :] = avg_chans
+        if right_pad:
+            te_im[:, c + left_pad:, :] = avg_chans
+        im_patch = te_im[int(context_ymin):int(context_ymax + 1),
+        int(context_xmin):int(context_xmax + 1), :]
     else:
-        image_patch = frame[int(y1):int(y2), int(x1):int(x2), :]
+        im_patch = im[int(context_ymin):int(context_ymax + 1),
+        int(context_xmin):int(context_xmax + 1), :]
 
-    image_patch = cv2.resize(image_patch, (size, size))
-
-    return image_patch
+    if not np.array_equal(model_sz, original_sz):
+        im_patch = cv2.resize(im_patch, (model_sz, model_sz))
+    im_patch = im_patch.transpose(2, 0, 1)
+    im_patch = im_patch[np.newaxis, :, :, :]
+    im_patch = im_patch.astype(np.float32)
+    im_patch = torch.from_numpy(im_patch)
+    # if cfg.CUDA:
+    #     im_patch = im_patch.cuda()
+    return im_patch
 
 
 def preprocess(img):
@@ -85,19 +139,28 @@ def update_bbox(prev_bbox, pos, response_size=8, stride=8):
     return new_cx, new_cy, 60, 60
 
 
-class SiamTracker:
+class NanoTracker:
     def __init__(self, model):
-        self.model = model
-        # self.template_feature = None
-        # self.original_template_feature = None
-        self.in_track, self.need_init = False, False
-        self.points = self.generate_points(stride=32, size=9)
+        self.size = None
+        self.center_pos = None
+        self.channel_average = None
+        self.need_init = False
 
-        # checkpoint = torch.load(MODEL_PATH, map_location='cpu')
-        # self.model.load_state_dict(checkpoint['state_dict'])
+        self.context_amount = 0.5
+        self.score_size = 15
+        self.stride = 16
+
+        hanning = np.hanning(self.score_size)
+        window = np.outer(hanning, hanning)
+        self.cls_out_channels = 2
+        self.window = window.flatten()
+
+        self.points = self.generate_points(self.stride, self.score_size)
+        self.model = model
         self.model.eval()
 
-    def generate_points(self, stride, size):
+    @staticmethod
+    def generate_points(stride, size):
         ori = - (size // 2) * stride
         x, y = np.meshgrid([ori + stride * dx for dx in np.arange(0, size)],
                            [ori + stride * dy for dy in np.arange(0, size)])
@@ -106,115 +169,124 @@ class SiamTracker:
 
         return points
 
-    def init(self, frame, bbox):
-        # self.center_pos = np.array([bbox[0] + (bbox[2] - 1) / 2, bbox[1] + (bbox[3] - 1) / 2])
-        # self.size = np.array([bbox[2], bbox[3]])
-        #
-        #
-        # cx, cy, w, h = bbox
-        #
-        # context_factor = 0.6
-        # context = context_factor * (w + h)
-        # crop_size = int(np.sqrt((w + context) * (h + context)))  # sqrt saves proportions
-        # channel_average = np.mean(frame, axis=(0, 1))
+    @staticmethod
+    def _convert_bbox(delta, point):
+        delta = delta.permute(1, 2, 3, 0).contiguous().view(4, -1)
+        delta = delta.detach().cpu().numpy()
 
+        delta[0, :] = point[:, 0] - delta[0, :] #x1
+        delta[1, :] = point[:, 1] - delta[1, :] #y1
+        delta[2, :] = point[:, 0] + delta[2, :] #x2
+        delta[3, :] = point[:, 1] + delta[3, :] #y2
+        delta[0, :], delta[1, :], delta[2, :], delta[3, :] = corner2center(delta)
+        return delta
 
+    def _convert_score(self, score):
+        if self.cls_out_channels == 1:
+            score = score.permute(1, 2, 3, 0).contiguous().view(-1)
+            score = score.sigmoid().detach().cpu().numpy()
+        else:
+            score = score.permute(1, 2, 3, 0).contiguous().view(self.cls_out_channels, -1).permute(1, 0)
+            score = score.softmax(1).detach()[:, 1].cpu().numpy()
+        return score
 
-        template = get_subwindow(frame, bbox, size=127)
-        template_tensor = preprocess(template)
-        with torch.no_grad():
-        #     self.template_feature = self.model.init(template)
-            self.model.init(template_tensor)
-            self.in_track = True
-        # self.original_template_feature = self.template_feature.clone()
-
-    def _bbox_clip(self, cx, cy, width, height, boundary):
+    @staticmethod
+    def _bbox_clip(cx, cy, width, height, boundary):
         cx = max(0, min(cx, boundary[1]))
         cy = max(0, min(cy, boundary[0]))
         width = max(10, min(width, boundary[1]))
         height = max(10, min(height, boundary[0]))
         return cx, cy, width, height
 
-    def track(self, frame, prev_bbox):
-        search = get_subwindow(frame, prev_bbox, size=255, context_factor=0.8)
-        search_tensor = preprocess(search)
+    def init(self, frame, bbox):
+        self.center_pos = np.array([bbox[0], bbox[1]])
+        self.size = np.array([bbox[2], bbox[3]])
 
-        print("search_tensor sum:", search_tensor.sum().item())
+        w = bbox[2]
+        h = bbox[3]
 
-        with torch.no_grad():
-            # search_feature = self.model.embedding_net(search)  # todo: use forward() instead
-            # response = self.model.match_corr(self.template_feature, search_feature)
-            response = self.model.track(search_tensor)['loc']
-            print(response.mean(), response.std())
+        context = self.context_amount * (w + h)
+        crop_size = int(np.sqrt((w + context) * (h + context)))  # sqrt saves proportions
+        self.channel_average = np.mean(frame, axis=(0, 1))
+
+        z_crop = get_subwindow_tracking(frame, self.center_pos, 127, crop_size, self.channel_average)
+
+        self.model.init(z_crop)
+
+    def track(self, img):
+        w_z = self.size[0] + self.context_amount * np.sum(self.size)
+        h_z = self.size[1] + self.context_amount * np.sum(self.size)
+        s_z = np.sqrt(w_z * h_z)
+        scale_z = 127 / s_z
+        s_x = s_z * (255 / 127)
+        x_crop = get_subwindow_tracking(img, self.center_pos, 255, round(s_x), self.channel_average)
+
+        outputs = self.model.track(x_crop)
+        score = self._convert_score(outputs['cls'])
+        pred_bbox = self._convert_bbox(outputs['loc'], self.points)
+
+        # print(pred_bbox)
+
+        def change(r):
+            return np.maximum(r, 1. / r)
+
+        def sz(w, h):
+            pad = (w + h) * 0.5
+            return np.sqrt((w + pad) * (h + pad))
+
+        # scale penalty
+        s_c = change(sz(pred_bbox[2, :], pred_bbox[3, :]) /
+                     (sz(self.size[0] * scale_z, self.size[1] * scale_z)))
+
+        # aspect ratio penalty
+        r_c = change((self.size[0] / self.size[1]) /
+                     (pred_bbox[2, :] / pred_bbox[3, :]))
+        penalty = np.exp(-(r_c * s_c - 1) * 0.138)
+
+        # score
+        pscore = penalty * score
+
+        # window penalty
+        pscore = pscore * (1 -  0.455) + \
+                 self.window * 0.455
+
+        best_idx = np.argmax(pscore)
+
+        bbox = pred_bbox[:, best_idx] / scale_z
+
+        lr = penalty[best_idx] * score[best_idx] * 0.348
+
+        cx = bbox[0] + self.center_pos[0]
+        cy = bbox[1] + self.center_pos[1]
+
+        # smooth bbox
+        width = self.size[0] * (1 - lr) + bbox[2] * lr
+        height = self.size[1] * (1 - lr) + bbox[3] * lr
+
+        # clip boundary
+        cx, cy, width, height = self._bbox_clip(cx, cy, width,
+                                                height, img.shape[:2])
+
+        # udpate state
+        self.center_pos = np.array([cx, cy])
+        self.size = np.array([width, height])
+
+        bbox = [cx - width / 2,
+                cy - height / 2,
+                width,
+                height]
+
+        best_score = score[best_idx]
+        return {
+            'bbox': bbox,
+            'best_score': best_score
+        }
 
 
-        pred_bbox = self._convert_bbox(response, self.points)
-
-        best_idx = np.argmax(pred_bbox[2, :] * pred_bbox[3, :])  # найбільший bbox як fallback
-
-        cx = pred_bbox[0, best_idx]
-        cy = pred_bbox[1, best_idx]
-        width = pred_bbox[2, best_idx]
-        height = pred_bbox[3, best_idx]
-
-        cx, cy, width, height = self._bbox_clip(cx, cy, width, height, frame.shape[:2])
-
-        new_bbox = [cx - width / 2, cy - height / 2, width, height]
-
-        # # normalization
-        # response = response['loc']
-        # response_np = response.squeeze().cpu().numpy()
-        # response_np = response_np - response_np.mean()
-        # response_np = response_np / (response_np.std() + 1e-8)
-        #
-        # # a bit correct to center
-        # h, w = response_np.shape
-        # hanning = np.outer(np.hanning(h), np.hanning(w))
-        # window_influence = 0.4
-        # response_np = (1 - window_influence) * response_np + window_influence * hanning
-        #
-        # pos = np.argmax(response_np)
-        #
-        # debug_norm = response_np - response_np.min()
-        # debug_norm = debug_norm / (debug_norm.max() + 1e-8)
-        # debug = cv2.resize(debug_norm, (200, 200), interpolation=cv2.INTER_NEAREST)
-        # debug = (debug * 255).astype(np.uint8)
-        # debug_color = cv2.applyColorMap(debug, cv2.COLORMAP_JET)
-        # cv2.imshow('response map', debug_color)
-        #
-        # new_bbox = update_bbox(prev_bbox, pos, response_size=h, stride=8)
-        #
-        # response_max = response_np.max()
-        # confidence_threshold = 2
-        #
-        # if response_max > confidence_threshold:
-        #     new_template = preprocess(get_subwindow(frame, new_bbox, size=127, context_factor=0.6))
-        #     with torch.no_grad():
-        #         new_feature = self.model.embedding_net(new_template)
-        #     alpha = 0.1
-        #     self.template_feature = alpha * new_feature + (1 - alpha) * self.template_feature
-        #
-        # gamma = 0.8
-        # self.template_feature = gamma * self.template_feature + (1 - gamma) * self.original_template_feature
-
-        return new_bbox
-
-    def _convert_bbox(self, delta, point):
-        delta = delta.permute(1, 2, 3, 0).contiguous().view(4, -1)
-        delta = delta.detach().cpu().numpy()
-
-        delta[0, :] = point[:, 0] - delta[0, :]  # x1
-        delta[1, :] = point[:, 1] - delta[1, :]  # y1
-        delta[2, :] = point[:, 0] + delta[2, :]  # x2
-        delta[3, :] = point[:, 1] + delta[3, :]  # y2
-        delta[0, :], delta[1, :], delta[2, :], delta[3, :] = corner2center(delta)
-        return delta
-
-
-    def on_mouse(self, event, x, y, _, __):
+    def on_mouse(self, event, cx, cy, _, __):
         if event == cv2.EVENT_LBUTTONDOWN:
             w, h = 60, 60
-            self.bbox = (x, y, w, h)
+            self.bbox = (cx, cy, w, h)
             self.need_init = True
 
 
